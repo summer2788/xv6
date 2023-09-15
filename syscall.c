358a5f0627e92e3016e5f66a78dcfe4d85463d5e
summer2788 <2019314658@swji.skku.edu>
2023-09-16 02:22:54 +0900

    did 3.Add extern and syscall element in syscall.c

--- syscall.c
+++ syscall.c
@@ -103,6 +103,10 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_getnice(void);
+extern int sys_setnice(void);
+extern void sys_ps(void);
+
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -126,6 +130,9 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_getnice] sys_getnice,
+[SYS_setnice] sys_setnice,
+[SYS_ps] sys_ps,
 };
 
 void
