9c26938aaf6333327ab6674b77a4acbd2db73b2f
summer2788 <2019314658@swji.skku.edu>
2023-09-16 02:30:57 +0900

    done 4 sysproc.c

--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,41 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_getnice(void)
+{
+    int pid;
+    if(argint(0, &pid) < 0)
+        return -1;
+    return getnice(pid);
+}
+
+
+int
+sys_setnice(void)
+{
+    int pid, value;
+    if(argint(0, &pid) < 0 || argint(1, &value) < 0)
+        return -1;
+    return setnice(pid, value);
+}
+
+
+void
+sys_ps(void)
+{
+    int pid;
+    if(argint(0, &pid) < 0)
+        return;
+    ps(pid);
+}
+
+
+
+
+
+
+
+
+
